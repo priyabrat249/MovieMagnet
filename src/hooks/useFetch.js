af1a2729845d69f33f6ab97ef6676a10b0e42172
priyabrat <priyabratswain2017@gmail.com>
2024-01-25 13:51:13 +0530

    Successfull

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+export const useFetch = (apiPath, queryTerm="") => {
+    // const [data, setData] = useState([]);
+    // const url = `https://api.themoviedb.org/3/${apiPath}?api_key=933ba33d8899e7b3e77466c9fd74c651&query=${queryTerm}`
+
+    // useEffect(() => {
+    //     async function fetchMovies(){
+    //       // const response = await fetch(url);
+    //       const json = await response.json();
+    //       setData(json.results);
+    //     }
+    //     fetchMovies();
+    //   }, [url])
+
+  // return { data }
+}
